522a520c1314b5b5c7aa35e458a57878e0e9fd0e
reggmack <thegraphixninja@gmail.com>
2018-05-17 19:58:07 -0500

    Constructors
    Constructors

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,30 +6,13 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var person = Person.Parse("John");
-            person.Introduce("Mack");
+            var customer = new Customer(1, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
