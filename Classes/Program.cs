fdb89bcd5c6435c11cfcdd658e14375b38b80ea0
reggmack <thegraphixninja@gmail.com>
2018-05-21 17:47:28 -0500

    Composition
    Composition

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -11,9 +11,14 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            var text = new Text();
-            text.Width = 100;
-            text.Copy();
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            
+            installer.Install();
         }
     }
 }
