4e00dcafe5d8c54e57e70c96daf7b2ec700aaa7d
reggmack <thegraphixninja@gmail.com>
2018-05-21 13:23:11 -0500

    Properties
    Properties

---
+++ Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public DateTime BirthDate { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,30 +6,14 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Person
-    {
-        private DateTime _birthDate;
-
-        public void SetBirthDate(DateTime birthdate)
-        {
-            _birthDate = birthdate;
-        }
-
-        public DateTime GetBirthDate()
-        {
-            return _birthDate;
-        }
-
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthDate(new DateTime(1982, 1, 1));
-            Console.WriteLine(person.GetBirthDate());
+           var person = new Person();
+            person.BirthDate = new DateTime(1982, 1, 1);
+
+            Console.WriteLine(person.Age);
         }
     }
 }
